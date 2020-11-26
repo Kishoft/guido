1b1c0f8848f703ab6769800d6d7bacfdd9bb3326
Ezequiel Regaldo <35587335+Kishoft@users.noreply.github.com>
2020-11-26 15:00:55 -0300

    Estilos

--- aplicaciones/trivia/script.js
+++ aplicaciones/trivia/script.js
@@ -5,6 +5,14 @@ let formulario = document.getElementById("trivia")
 
 
 class Trivia {
+
+    constructor(){
+
+        Trivia.inyectarPregunta()
+        Trivia.inyectarProgreso()
+    }
+
+    //Objeto que tiene las preguntas, sus respuestas y qué puntaje otorga cada respuesta
     static items = [
         {
             pregunta : "¿De qué color era el caballo blanco de San Martín?",
@@ -75,23 +83,23 @@ class Trivia {
             ]
         }
     ]
-
-    static preguntasRealizadas = [];
+    //Inicializamos unas variables para que funcione el programa. Le damos un estado inicial
     static puntaje = 0;
     static progreso = 0;
     static cantidadDePreguntas = 4;
-
+    //Generamos aleatoriamente un índice para seleccionar "aleatoriamente" una pregunta
+    //Simplemente nos devolverá un número 
     static generarIndiceAleatorio(){
-
-        let indiceDeLaPregunta;
+        //Creamos el valor del índice dentro de la función así se recicla y no interfiere con otra parte de la aplicación
+        let indiceDeLaPregunta = 0;
 
         do {
+            //Nos da un número entre 0 y 1, luego * 10 para desplazar la coma, y el tofixed para quitarle las decimales, y como eso devuelve un string, lo convertimos a number
             indiceDeLaPregunta = Number((Math.random() * 10).toFixed(0))
+            //En el bucle, nos estamos asegurando de que el número corresponda a un índice válido de nuestro arreglo de preguntas y respuestas
         } 
         while (this.items.length <= indiceDeLaPregunta);
-        
-        this.preguntasRealizadas.push(indiceDeLaPregunta)
-
+        //Devolvemos el resultado
         return indiceDeLaPregunta
     }
 
@@ -104,58 +112,68 @@ class Trivia {
     }
 
     static inyectarPregunta(){
+        //Nos traemos una pregunta de forma aleatoria
         let preguntaSeleccionada = this.generarPregunta()
-
+        //Metemos un título de fieldset con la pregunta (texto)
         preguntasContainer.innerHTML = `<legend>${preguntaSeleccionada.pregunta}</legend>`
-
-
+        //Iteramos las posibles respuestas
         for (let i = 0; i < preguntaSeleccionada.respuestas.length; i++) {
+            //Creamos en memoria un elemento label con sus propiedades/atributos
+            let fieldsetContainer = document.createElement("div")
+            fieldsetContainer.classList.add("inputContainer")
             let label = document.createElement("label")
             label.textContent = preguntaSeleccionada.respuestas[i].valor
             label.htmlFor = `pregunta${i}`
+            //Lo mismo que el anterior pero con un input
             let input = document.createElement("input")
             input.required = true
             input.id = `pregunta${i}`
             input.type = "radio"
             input.name = "respuesta"
             input.value = preguntaSeleccionada.respuestas[i].puntaje
-
-            preguntasContainer.append(input, label)
-            
+            //Llamamos a la función append para inyectar los elementos de la memoria a la web
+            fieldsetContainer.append(input, label)
+            preguntasContainer.append(fieldsetContainer)
         }
-
-        preguntasContainer.innerHTML += '<br><input type="submit" value="Continuar">'
+        //Y para finalizar inyectamos con html crudo un botón que sirve para enviar la información del formulario
+        preguntasContainer.innerHTML += '<input type="submit" value="Continuar">'
     }
 
     static finalizarJuego(){
-
+        //Creamos elementos en memoria
         let legend = document.createElement("legend")
         legend.textContent = "Juego finalizado";
         let input = document.createElement("input")
         input.type = "submit";
         input.value = "Jugar otra vez";
+        //Le estamos agregando una escucha de evento al botón para que limpie las variables de nuestra aplicación
         input.addEventListener("click", e => {
             e.preventDefault()
-            this.preguntasRealizadas = [];
             this.puntaje = 0;
             this.progreso = 0;
             this.inyectarPregunta()
+            this.inyectarPuntaje()
+            this.inyectarProgreso()
         })
-
+        //Limpiamos la vista (web, html) del contenedor
         preguntasContainer.innerHTML = "";
+        //Luego inyectamos el aviso de que finalizó el juego
         preguntasContainer.append(legend, input)
 
     }
 
     static actualizarProgreso(){
+        //Aumenta la variable progreso en una unidad
         this.progreso++
+        //Luego verifica que no hayamos llegado al límite de preguntas por partida
         if(this.progreso >= this.cantidadDePreguntas){
             this.finalizarJuego()
         }
         else{
+            //Si no llega al límite, continúa con la siguiente pregunta
             this.inyectarPregunta()
         }
-
+        //Refresca el progreso
         this.inyectarProgreso()
 
     }
@@ -165,17 +183,23 @@ class Trivia {
     static actualizarPuntaje(puntos){
         puntos = Number(puntos);
         this.puntaje += puntos;
-        puntajeOutput.value = this.puntaje;
+        this.inyectarPuntaje()
         this.actualizarProgreso()
+    }
+    static inyectarPuntaje(){
+        puntajeOutput.value = this.puntaje;
     }
 }
 
-formulario.addEventListener("submit", e => {
-    
-    e.preventDefault();
+//Vamos a escuchar el evento submit del formulario, quiere decir que cuando haya un input del tipo submit y sea clickeado, sucederá
+//Lo que dice esta función. 
 
+formulario.addEventListener("submit", e => {
+    //Prevenimos que se envíe el formulario porque vamos a manipular la información nosotros
+    e.preventDefault();
+    //Actualizamos el puntaje
     Trivia.actualizarPuntaje(formulario.respuesta.value)
 })
+//Inicialización de la aplicación
 
-Trivia.inyectarPregunta()
-Trivia.inyectarProgreso()
+new Trivia
