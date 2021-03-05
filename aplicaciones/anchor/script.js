19c3c20b1fa6683211c64d4d2a72920627b7525c
Ashura3 <74105584+Ashura3@users.noreply.github.com>
2021-03-05 12:28:43 -0300

    terminado

--- aplicaciones/anchor/script.js
+++ aplicaciones/anchor/script.js
@@ -1,8 +1,30 @@
 let aElements = document.getElementsByTagName("a");
 
+let observador = new IntersectionObserver(changes => {
+    console.log(changes)
+    changes.forEach(change => {
+        if (change.isIntersecting) {
+            for (const a of aElements) {
+                let link = a.hash.slice(1)
+                if (link === change.target.id) {
+                    a.style.background = "orangered"
+                }
+            }
+        }
+        else {
+            for (const a of aElements) {
+                let link = a.hash.slice(1)
+                if (link === change.target.id) {
+                    a.style.background = "transparent"
+                }
+            }
+        }
+    });
+}, { threshold: 0.5 });
+
 for (let a of aElements) {
-    
+
     let elementoAnclado = document.getElementById(a.hash.slice(1))
 
-    console.log(elementoAnclado)
-}
+    observador.observe(elementoAnclado)
+};
